5689aabde2435cbc15b9f6ebc0c696be308cdec6
bkmrdchlv <bek.murod.achilov2@gmail.com>
2024-03-15 06:57:08 -0400

    A Tour of Computer Systems/1.1

---
+++ A Tour of Computer Systems/1.1/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello world\n");
+    return 0;
+}
+
+// how compile code 
+// hello.c -> hello.i -> hello.s -> hello.o -> hello
+
+// source code = hello.c
+// modificated source programm = hello.i
+// programm on assembler = hello.s
+// object code = hello.o
+// executable object code = hello
